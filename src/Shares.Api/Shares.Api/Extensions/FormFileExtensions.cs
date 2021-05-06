ce7e3cc6a22e17b3df7798dfad1c14ac2bfce305
mrunal-patil <mrunalnpatil1@gmail.com>
2021-05-06 23:26:33 +0200

    Add transactions controller and validate if the file uploaded is an excel file

---
+++ src/Shares.Api/Shares.Api/Extensions/FormFileExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shares.Api.Extensions
+{
+    public static class FormFileExtensions
+    {
+        public static bool IsValid(this IFormFile file)
+        {
+            if (Path.GetExtension(file.FileName).Trim().ToLowerInvariant() != ".xlsx")
+                return false;
+
+            return true;
+        }
+    }
+}
