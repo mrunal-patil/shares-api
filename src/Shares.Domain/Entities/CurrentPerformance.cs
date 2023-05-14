9dc4a776b480247e904d8917a7e876f10af236ad
Mrunal <mrunal.patil@coolblue.nl>
2023-05-14 12:14:45 +0200

    Provide lowest value in the current cycle instead of percentage value

--- src/Shares.Domain/Entities/CurrentPerformance.cs
+++ src/Shares.Domain/Entities/CurrentPerformance.cs
@@ -8,18 +8,18 @@ namespace Shares.Domain.Entities
             string ticker,
             decimal todaysValue,
             decimal peakValue,
-            decimal dropInCurrentCycleFromLowestPoint)
+            decimal lowestValueInCurrentCycle)
         {
             Ticker = ticker;
             TodaysValue = Math.Round(todaysValue, 2);
             PeakValue = Math.Round(peakValue, 2);
-            DropInCurrentCycleFromLowestPoint = Math.Round(dropInCurrentCycleFromLowestPoint, 2);
+            LowestValueInCurrentCycle = Math.Round(lowestValueInCurrentCycle, 2);
         }
 
         public string Ticker { get; }
         public decimal TodaysValue { get; }
         public decimal PeakValue { get; }
-        public decimal DropInCurrentCycleFromLowestPoint { get; }
+        public decimal LowestValueInCurrentCycle { get; }
         public decimal DropInCurrentCycleToday => Math.Round(1 - TodaysValue / PeakValue, 2);
     }
 }
